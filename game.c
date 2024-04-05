c53886d5315527e18cde354e980d770fb12cfd2c
Yehonatan Katz <yehonatankatz@gmail.com>
2024-04-05 01:02:45 -0400

    h

--- game.c
+++ game.c
@@ -3,37 +3,72 @@
 
 #define LIGHTBLUE ColorFromHSV(200, 0.78, 0.92)
 
-enum PixelType {
+enum ParticleType {
 	DIRT,
-	AIR
+	AIR,
+	EMPTY
 };
 
 typedef struct {
-	enum PixelType type;
-} Pixel;
+	enum ParticleType type;
+	enum ParticleType next_type;
+	int updated;
+} Particle;
 
-void generate_world(Pixel **world, int world_size) {
+void generate_world(Particle **world, int world_size) {
 	for (int i = 0; i < world_size/2; i++) {
 		for (int j = 0; j < world_size; j++) {
-			world[i][j].type = AIR;	
+			if (j % 3 == 0) {
+				world[i][j].type = AIR;
+			}
+			else {
+				world[i][j].type = DIRT;	
+			}
+			world[i][j].updated = 0;	
 		}
 	}
 	for (int i = world_size/2; i < world_size; i++) {
 		for (int j = 0; j < world_size; j++) {
 			world[i][j].type = DIRT;	
+			world[i][j].updated = 0;	
 		}
+	}
+}
+
+Color color_lookup(enum ParticleType type) {
+	if (type == AIR) return LIGHTBLUE;
+	if (type == DIRT) return DARKBROWN;
+	else return RED;
+}
+
+void update_me(Particle **world, int x, int y, int world_size) {
+	enum ParticleType my_type = world[y][x].type;
+	enum ParticleType below = EMPTY;
+	if (y < world_size - 1) {
+		below = world[y+1][x].type;
+	}
+	else {
+		return;
+	}
+	if (my_type == AIR && below == DIRT) {
+		world[y+1][x].next_type = AIR;
+		world[y+1][x].updated = 1;
+	}
+	DrawPixel(x, y, color_lookup(world[y][x].type));
+	if (world[y][x].updated) {
+		world[y][x].type = world[y][x].next_type;
+		world[y][x].updated = 0;
 	}
 }
 
 int main() {
 	int world_size = 800;
 	InitWindow(world_size, world_size, "World");
-	SetTargetFPS(60);
-	Color color_lookup[2] = {DARKBROWN, LIGHTBLUE};
+	SetTargetFPS(10);
 	
-	Pixel *_world = (Pixel*)malloc(world_size * world_size * sizeof(Pixel));
+	Particle *_world = (Particle*)malloc(world_size * world_size * sizeof(Particle));
 	if (!_world) return -1;
-	Pixel **world = (Pixel**)malloc(world_size * sizeof(Pixel*));
+	Particle **world = (Particle**)malloc(world_size * sizeof(Particle*));
 	if (!world) {
 		free(_world);
 		return -1;
@@ -48,12 +83,10 @@ int main() {
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(DARKGRAY);
-		int x;
-		int y;
-		for (int i = 0; i < world_size*world_size; i++) {
-			x = i % world_size;
-			y = i / world_size;
-			DrawPixel(x, y, color_lookup[world[y][x].type]);
+		for (int i = 0; i < world_size; i++) {
+			for (int j = 0; j < world_size; j++) {
+				update_me(world, j, i, world_size);
+			}
 		}
 		EndDrawing();
 	}
