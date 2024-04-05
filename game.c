fc99fd00a7b651261d1d7c270e6e7a99839fa723
Yehonatan Katz <yehonatankatz@gmail.com>
2024-04-05 00:03:03 -0400

    ah

--- game.c
+++ game.c
@@ -12,6 +12,19 @@ typedef struct {
 	enum PixelType type;
 } Pixel;
 
+void generate_world(Pixel **world, int world_size) {
+	for (int i = 0; i < world_size/2; i++) {
+		for (int j = 0; j < world_size; j++) {
+			world[i][j].type = AIR;	
+		}
+	}
+	for (int i = world_size/2; i < world_size; i++) {
+		for (int j = 0; j < world_size; j++) {
+			world[i][j].type = DIRT;	
+		}
+	}
+}
+
 int main() {
 	int world_size = 800;
 	InitWindow(world_size, world_size, "World");
@@ -19,20 +32,18 @@ int main() {
 	Color color_lookup[2] = {DARKBROWN, LIGHTBLUE};
 	
 	Pixel *_world = (Pixel*)malloc(world_size * world_size * sizeof(Pixel));
+	if (!_world) return -1;
 	Pixel **world = (Pixel**)malloc(world_size * sizeof(Pixel*));
+	if (!world) {
+		free(_world);
+		return -1;
+	}
 	for (int i = 0; i < world_size; i++) {
 		world[i] = &(_world[i*world_size]);
 	}
-	// World Generation
-	for (int i = 0; i < world_size*world_size; i++) {
-		if (i < (world_size*world_size) / 2) {
-			_world[i].type = AIR;
-		}
-		else {
-			_world[i].type = DIRT;
-		}
-	}
 
+	// World Generation
+	generate_world(world, world_size);
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
