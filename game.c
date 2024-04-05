0be363427b5d6fbad9a7c47bb6eea48a86db59a4
Yehonatan Katz <yehonatankatz@gmail.com>
2024-04-05 01:30:20 -0400

    h

--- game.c
+++ game.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "raylib.h"
 
@@ -5,6 +6,7 @@
 
 enum ParticleType {
 	DIRT,
+	SAND,
 	AIR,
 	EMPTY
 };
@@ -16,45 +18,59 @@ typedef struct {
 } Particle;
 
 void generate_world(Particle **world, int world_size) {
-	for (int i = 0; i < world_size/2; i++) {
+	for (int i = 0; i < world_size; i++) {
 		for (int j = 0; j < world_size; j++) {
-			if (j % 3 == 0) {
-				world[i][j].type = AIR;
+			if (i > 200 && i < 400 && j > 200 && j < 400) {
+
+				world[i][j].type = SAND;	
+				world[i][j].updated = 0;	
 			}
 			else {
-				world[i][j].type = DIRT;	
+				world[i][j].type = EMPTY;	
+				world[i][j].updated = 0;	
 			}
-			world[i][j].updated = 0;	
-		}
-	}
-	for (int i = world_size/2; i < world_size; i++) {
-		for (int j = 0; j < world_size; j++) {
-			world[i][j].type = DIRT;	
-			world[i][j].updated = 0;	
 		}
 	}
 }
 
 Color color_lookup(enum ParticleType type) {
 	if (type == AIR) return LIGHTBLUE;
-	if (type == DIRT) return DARKBROWN;
+	else if (type == DIRT) return DARKBROWN;
+	else if (type == SAND) return BROWN;
+	else if (type == EMPTY) return BLACK;
 	else return RED;
 }
 
-void update_me(Particle **world, int x, int y, int world_size) {
-	enum ParticleType my_type = world[y][x].type;
-	enum ParticleType below = EMPTY;
-	if (y < world_size - 1) {
-		below = world[y+1][x].type;
-	}
-	else {
+void update_sand(Particle **world, int x, int y, int world_size) {
+	if (y < world_size-1 && world[y+1][x].type == EMPTY) {
+		world[y+1][x].next_type = SAND;
+		world[y+1][x].updated = 1;
+		world[y][x].next_type = EMPTY;
+		world[y][x].updated = 1;
 		return;
 	}
-	if (my_type == AIR && below == DIRT) {
-		world[y+1][x].next_type = AIR;
-		world[y+1][x].updated = 1;
+	else if (y < world_size-1 && x > 1 && world[y+1][x-1].type == EMPTY) {
+		world[y+1][x-1].next_type = SAND;
+		world[y+1][x-1].updated = 1;
+		world[y][x].next_type = EMPTY;
+		world[y][x].updated = 1;
+		return;
 	}
-	DrawPixel(x, y, color_lookup(world[y][x].type));
+	else if (y < world_size-1 && x < world_size - 1 && world[y+1][x+1].type == EMPTY) {
+		world[y+1][x+1].next_type = SAND;
+		world[y+1][x+1].updated = 1;
+		world[y][x].next_type = EMPTY;
+		world[y][x].updated = 1;
+		return;
+	}
+}
+void update_me(Particle **world, int x, int y, int world_size) {
+	enum ParticleType my_type = world[y][x].type;
+	if (my_type == SAND) {
+		update_sand(world, x, y, world_size);
+	}
+}
+void update_apply(Particle **world, int x, int y, int world_size) {
 	if (world[y][x].updated) {
 		world[y][x].type = world[y][x].next_type;
 		world[y][x].updated = 0;
@@ -64,7 +80,7 @@ void update_me(Particle **world, int x, int y, int world_size) {
 int main() {
 	int world_size = 800;
 	InitWindow(world_size, world_size, "World");
-	SetTargetFPS(10);
+	SetTargetFPS(60);
 	
 	Particle *_world = (Particle*)malloc(world_size * world_size * sizeof(Particle));
 	if (!_world) return -1;
@@ -86,6 +102,12 @@ int main() {
 		for (int i = 0; i < world_size; i++) {
 			for (int j = 0; j < world_size; j++) {
 				update_me(world, j, i, world_size);
+				DrawPixel(j, i, color_lookup(world[i][j].type));
+			}
+		}
+		for (int i = 0; i < world_size; i++) {
+			for (int j = 0; j < world_size; j++) {
+				update_apply(world, j, i, world_size);
 			}
 		}
 		EndDrawing();
