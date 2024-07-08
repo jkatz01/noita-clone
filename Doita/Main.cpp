1c76757a0fbb5a2fc0c136414204a8798961dfe6
Yehonatan Katz <yehonatankatz@gmail.com>
2024-07-08 17:21:12 -0400

    now its in C++

--- Doita/Main.cpp
+++ Doita/Main.cpp
@@ -1,22 +1,17 @@
-/*
- * by: dominus
- * heheuhh
- */
+#include <iostream>
+#include "raylib.h"
 
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 #include "raylib.h"
 
-#define LIGHTBLUE ColorFromHSV(200, 0.78, 0.92)
+#define LIGHTBLUE ColorFromHSV(200, 0.78f, 0.92f)
 #define JUICERED CLITERAL(Color){200, 100, 2, 255}
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
 #define WATER_BLUE       CLITERAL(Color){ 0, 121, 241, 150 } 
 
-const int world_size = 800;
+const int world_size = 200;
 const int screen_size = 800;
 const int scaled_size = screen_size / world_size;
 
@@ -31,7 +26,7 @@ enum ParticleType {
 	EMPTY
 };
 
-typedef struct  {
+typedef struct {
 	int up_left, left, down_left, down, down_right, right, up_right, up;
 } PixelSurroundings;
 
@@ -42,6 +37,7 @@ typedef struct {
 	Vector2 velocity;
 } Particle;
 
+
 float rand_range(float min, float max)
 {
 	float random = (float)rand() / RAND_MAX;
@@ -51,29 +47,29 @@ float rand_range(float min, float max)
 
 Color color_lookup(enum ParticleType type) {
 	switch (type) {
-		case AIR: return LIGHTBLUE;
-		case STONE: return DARKBROWN;
-		case SAND: return ColorBrightness(BROWN, rand_range(-0.3, 0.3));
-		case WATER: return ColorBrightness(WATER_BLUE, rand_range(-0.1, 0.1));
-		case ORANGE_JUICE: return ColorBrightness(JUICERED, rand_range(0, 0.3));
-		case EMPTY: return CLITERAL(Color){ 0, 0, 0, 100 } ;
-		default: return RED;
+	case AIR: return LIGHTBLUE;
+	case STONE: return DARKBROWN;
+	case SAND: return ColorBrightness(BROWN, rand_range(-0.3f, 0.3f));
+	case WATER: return ColorBrightness(WATER_BLUE, rand_range(-0.1f, 0.1f));
+	case ORANGE_JUICE: return ColorBrightness(JUICERED, rand_range(0, 0.3f));
+	case EMPTY: return CLITERAL(Color) { 0, 0, 0, 100 };
+	default: return RED;
 	}
 }
 
 int density_lookup(enum ParticleType type) {
 	switch (type) {
-		case AIR: return 0;
-		case STONE: return 3;
-		case SAND: return 3;
-		case WATER: return 1;
-		case ORANGE_JUICE: return 2;
-		case EMPTY: return 0;
-		default: return 0;
+	case AIR: return 0;
+	case STONE: return 3;
+	case SAND: return 3;
+	case WATER: return 1;
+	case ORANGE_JUICE: return 2;
+	case EMPTY: return 0;
+	default: return 0;
 	}
 }
 
-void generate_world(Particle **world, int world_size) {
+void generate_world(Particle** world, int world_size) {
 	for (int i = 0; i < world_size; i++) {
 		for (int j = 0; j < world_size; j++) {
 			world[i][j].updated = 0;
@@ -82,7 +78,7 @@ void generate_world(Particle **world, int world_size) {
 				world[i][j].color = color_lookup(SAND);
 			}
 			else {
-				world[i][j].type = EMPTY;	
+				world[i][j].type = EMPTY;
 				world[i][j].color = color_lookup(EMPTY);
 			}
 		}
@@ -95,7 +91,7 @@ int three_min(int x, int y, int z) {
 	return val;
 }
 
-void add_material(Particle **world, int x, int y, int world_size, int brush_size, enum ParticleType mt_type, int mouse_held) {
+void add_material(Particle** world, int x, int y, int world_size, int brush_size, enum ParticleType mt_type, int mouse_held) {
 	static int previous_x, previous_y;
 	if (mouse_held == 0) {
 		previous_x = x;
@@ -107,37 +103,37 @@ void add_material(Particle **world, int x, int y, int world_size, int brush_size
 	if (x < 0 || x >= world_size) {
 		return;
 	}
-	
-	if (previous_x == x  && previous_y == y) {
-		x += brush_size/scaled_size;
-		y += brush_size/scaled_size;
+
+	if (previous_x == x && previous_y == y) {
+		x += brush_size / scaled_size;
+		y += brush_size / scaled_size;
 	}
-	
-	Vector2 startPos = {x*scaled_size, y*scaled_size};
-	Vector2 endPos = {previous_x*scaled_size, previous_y*scaled_size};
-	
+
+	Vector2 startPos = { (float)x * scaled_size, (float)y * scaled_size };
+	Vector2 endPos = { (float)previous_x * scaled_size, (float)previous_y * scaled_size };
+
 
 
 	for (int i = 0; i < world_size; i++) {
 		for (int j = 0; j < world_size; j++) {
-			DrawRectangleLines(MIN(previous_x, x)*scaled_size, MIN(previous_y, y)*scaled_size, (MAX(previous_x, x) - MIN(previous_x, x))*scaled_size, (MAX(previous_y, y) - MIN(previous_y, y))*scaled_size, GREEN);
-			if (CheckCollisionPointLine((Vector2){ j*scaled_size, i*scaled_size } ,startPos, endPos, brush_size)) {
-				if(world[i][j].type == EMPTY) {
+			DrawRectangleLines(MIN(previous_x, x) * scaled_size, MIN(previous_y, y) * scaled_size, (MAX(previous_x, x) - MIN(previous_x, x)) * scaled_size, (MAX(previous_y, y) - MIN(previous_y, y)) * scaled_size, GREEN);
+			if (CheckCollisionPointLine( Vector2 { (float)j * scaled_size, (float)i* scaled_size }, startPos, endPos, brush_size)) {
+				if (world[i][j].type == EMPTY) {
 					world[i][j].type = mt_type;
 					world[i][j].color = color_lookup(mt_type);
 				}
 			}
-			
+
 		}
 	}
 
 	previous_x = x;
 	previous_y = y;
-	
+
 }
 
 
-void delete_material(Particle **world, int x, int y, int world_size, int brush_size, int mouse_held) {
+void delete_material(Particle** world, int x, int y, int world_size, int brush_size, int mouse_held) {
 	static int previous_x, previous_y;
 	if (mouse_held == 0) {
 		previous_x = x;
@@ -149,22 +145,22 @@ void delete_material(Particle **world, int x, int y, int world_size, int brush_s
 	if (x < 0 || x >= world_size) {
 		return;
 	}
-	if (previous_x == x  && previous_y == y) {
-		x += brush_size/scaled_size;
-		y += brush_size/scaled_size;
+	if (previous_x == x && previous_y == y) {
+		x += brush_size / scaled_size;
+		y += brush_size / scaled_size;
 	}
-	
-	Vector2 startPos = {x*scaled_size, y*scaled_size};
-	Vector2 endPos = {previous_x*scaled_size, previous_y*scaled_size};
+
+	Vector2 startPos = { (float)x * scaled_size, (float)y * scaled_size };
+	Vector2 endPos = { (float)previous_x * scaled_size, (float)previous_y * scaled_size };
 
 	for (int i = 0; i < world_size; i++) {
 		for (int j = 0; j < world_size; j++) {
-			DrawRectangleLines(MIN(previous_x, x)*scaled_size, MIN(previous_y, y)*scaled_size, (MAX(previous_x, x) - MIN(previous_x, x))*scaled_size, (MAX(previous_y, y) - MIN(previous_y, y))*scaled_size, GREEN);
-			if (CheckCollisionPointLine((Vector2){ j*scaled_size, i*scaled_size } ,startPos, endPos, brush_size)) {
+			DrawRectangleLines(MIN(previous_x, x) * scaled_size, MIN(previous_y, y) * scaled_size, (MAX(previous_x, x) - MIN(previous_x, x)) * scaled_size, (MAX(previous_y, y) - MIN(previous_y, y)) * scaled_size, GREEN);
+			if (CheckCollisionPointLine( Vector2 { (float)j* scaled_size, (float)i* scaled_size }, startPos, endPos, brush_size)) {
 				world[i][j].type = EMPTY;
 				world[i][j].color = color_lookup(EMPTY);
 			}
-			
+
 		}
 	}
 
@@ -173,72 +169,72 @@ void delete_material(Particle **world, int x, int y, int world_size, int brush_s
 }
 //up_left, , , , , , up_right, up;
 PixelSurroundings check_bounds(int x, int y, int world_size) {
-	PixelSurroundings me = {0, 0, 0, 0, 0, 0, 0, 0};
+	PixelSurroundings me = { 0, 0, 0, 0, 0, 0, 0, 0 };
 	if (y < world_size - 1) me.down = 1;
 	if (x > 0) me.left = 1;
 	if (y < world_size - 1 && x > 0) me.down_left = 1;
-	if (x < world_size-1) me.right = 1;
+	if (x < world_size - 1) me.right = 1;
 	if (y < world_size - 1 && x < world_size - 1) me.down_right = 1;
 	return me;
 }
 
-void move_down(Particle **world, int x, int y) {
-	Particle r_part = world[y+1][x];
-	world[y+1][x] = world[y][x];
+void move_down(Particle** world, int x, int y) {
+	Particle r_part = world[y + 1][x];
+	world[y + 1][x] = world[y][x];
 	//world[y+1][x].updated = 1;
 	world[y][x] = r_part;
 }
-void move_down_right(Particle **world, int x, int y) {
-	Particle r_part = world[y+1][x+1];
-	world[y+1][x+1] = world[y][x];
+void move_down_right(Particle** world, int x, int y) {
+	Particle r_part = world[y + 1][x + 1];
+	world[y + 1][x + 1] = world[y][x];
 	//world[y+1][x+1].updated = 1;
 	world[y][x] = r_part;
 }
-void move_down_left(Particle **world, int x, int y) {
-	Particle r_part = world[y+1][x-1];
-	world[y+1][x-1] = world[y][x];
+void move_down_left(Particle** world, int x, int y) {
+	Particle r_part = world[y + 1][x - 1];
+	world[y + 1][x - 1] = world[y][x];
 	//world[y+1][x-1].updated = 1;
 	world[y][x] = r_part;
 }
-void move_right(Particle **world, int x, int y) {
-	Particle r_part = world[y][x+1];
-	world[y][x+1] = world[y][x];
+void move_right(Particle** world, int x, int y) {
+	Particle r_part = world[y][x + 1];
+	world[y][x + 1] = world[y][x];
 	//world[y][x+1].updated = 1;
 	world[y][x] = r_part;
 }
-void move_left(Particle **world, int x, int y) {
-	
-	Particle r_part = world[y][x-1];
-	world[y][x-1] = world[y][x];
+void move_left(Particle** world, int x, int y) {
+
+	Particle r_part = world[y][x - 1];
+	world[y][x - 1] = world[y][x];
 	//world[y][x-1].updated = 1;
 	world[y][x] = r_part;
 }
 
-void move_by_velocity(Particle **world, int x, int y, Vector2 vel) {
+void move_by_velocity(Particle** world, int x, int y, Vector2 vel) {
 	int cur_x = x;
 	int cur_y = y;
-	
-	if (vel.y > 1 && cur_y < world_size-2 && density_lookup(world[cur_y][cur_x].type) - density_lookup(world[cur_y-1][cur_x].type) == 1) {
+
+	if (vel.y > 1 && cur_y < world_size - 2 && density_lookup(world[cur_y][cur_x].type) - density_lookup(world[cur_y - 1][cur_x].type) == 1) {
 		if (x % 2 == 0) vel.x += 1;
 		else 			vel.x -= 1;
 		vel.y -= 1;
 	}
-	
+
 	int desired_x = x + (int)vel.x;
 	int desired_y = y + (int)vel.y;
-	
-	
-	if (desired_x > world_size) desired_x=world_size;
+
+
+	if (desired_x > world_size) desired_x = world_size;
 	if (desired_x < 0) 			desired_x = 0;
-	if (desired_y > world_size-1) desired_y=world_size-1;
+	if (desired_y > world_size - 1) desired_y = world_size - 1;
 	if (desired_y < 0) 			desired_y = 0;
-	
-	
-	
+
+
+
 	// X direction
 	if (vel.x < 0) {
 		while (cur_x != desired_x) {
-			if (density_lookup(world[cur_y][cur_x-1].type) < density_lookup(world[cur_y][cur_x].type)) {
+			if (density_lookup(world[cur_y][cur_x - 1].type) < density_lookup(world[cur_y][cur_x].type)) {
 				move_left(world, cur_x, cur_y);
 				cur_x--;
 			}
@@ -247,7 +243,7 @@ void move_by_velocity(Particle **world, int x, int y, Vector2 vel) {
 	}
 	else if (vel.x > 0) {
 		while (cur_x != desired_x) {
-			if (density_lookup(world[cur_y][cur_x+1].type) < density_lookup(world[cur_y][cur_x].type)) {
+			if (density_lookup(world[cur_y][cur_x + 1].type) < density_lookup(world[cur_y][cur_x].type)) {
 				move_right(world, cur_x, cur_y);
 				cur_x++;
 			}
@@ -262,7 +258,7 @@ void move_by_velocity(Particle **world, int x, int y, Vector2 vel) {
 	}
 	else if (vel.y > 0) {
 		while (cur_y != desired_y) {
-			if (density_lookup(world[cur_y+1][cur_x].type) < density_lookup(world[cur_y][cur_x].type)) {
+			if (density_lookup(world[cur_y + 1][cur_x].type) < density_lookup(world[cur_y][cur_x].type)) {
 				move_down(world, cur_x, cur_y);
 				cur_y++;
 			}
@@ -272,128 +268,128 @@ void move_by_velocity(Particle **world, int x, int y, Vector2 vel) {
 	world[cur_y][cur_x].updated = 1;
 }
 
-void update_powder(Particle **world, int x, int y, int world_size) {
+void update_powder(Particle** world, int x, int y, int world_size) {
 	PixelSurroundings my_pos = check_bounds(x, y, world_size);
-	if (my_pos.down && density_lookup(world[y+1][x].type) < density_lookup(world[y][x].type)) {
-		if (world[y+1][x].updated == 1) {
+	if (my_pos.down && density_lookup(world[y + 1][x].type) < density_lookup(world[y][x].type)) {
+		if (world[y + 1][x].updated == 1) {
 			return;
 		}
-		Vector2 test_vel = {0, 3};
+		Vector2 test_vel = { 0, 3 };
 		move_by_velocity(world, x, y, test_vel);
 		return;
 	}
-	else if (my_pos.down_left &&density_lookup(world[y+1][x-1].type) < density_lookup(world[y][x].type)) {
-		if (world[y+1][x-1].updated == 1) {
+	else if (my_pos.down_left && density_lookup(world[y + 1][x - 1].type) < density_lookup(world[y][x].type)) {
+		if (world[y + 1][x - 1].updated == 1) {
 			return;
 		}
-		Vector2 test_vel = {-2, 3};
+		Vector2 test_vel = { -2, 3 };
 		move_by_velocity(world, x, y, test_vel);
 		return;
 	}
-	else if (my_pos.down_right &&density_lookup(world[y+1][x+1].type) < density_lookup(world[y][x].type)) {
-		if (world[y+1][x+1].updated == 1) {
+	else if (my_pos.down_right && density_lookup(world[y + 1][x + 1].type) < density_lookup(world[y][x].type)) {
+		if (world[y + 1][x + 1].updated == 1) {
 			return;
 		}
-		Vector2 test_vel = {2, 3};
+		Vector2 test_vel = { 2, 3 };
 		move_by_velocity(world, x, y, test_vel);
 		return;
-	} 
+	}
 }
-void update_liquid(Particle **world, int x, int y, int world_size) {
+void update_liquid(Particle** world, int x, int y, int world_size) {
 	PixelSurroundings my_pos = check_bounds(x, y, world_size);
 	//int odd = x % 2;
 	//if odd()
-	if (my_pos.down &&density_lookup(world[y+1][x].type) < density_lookup(world[y][x].type)) {
-		Vector2 test_vel = {0, 3};
+	if (my_pos.down && density_lookup(world[y + 1][x].type) < density_lookup(world[y][x].type)) {
+		Vector2 test_vel = { 0, 3 };
 		move_by_velocity(world, x, y, test_vel);
 		return;
 	}
-	
-	if (my_pos.down_right &&density_lookup(world[y+1][x+1].type) < density_lookup(world[y][x].type)) {
-		Vector2 test_vel = {4, 3};
-		move_by_velocity(world, x, y, test_vel);
-		return;
-	} 
-	if (my_pos.down_left &&density_lookup(world[y+1][x-1].type) < density_lookup(world[y][x].type)) {
-		Vector2 test_vel = {-4, 3};
+
+	if (my_pos.down_right && density_lookup(world[y + 1][x + 1].type) < density_lookup(world[y][x].type)) {
+		Vector2 test_vel = { 4, 3 };
 		move_by_velocity(world, x, y, test_vel);
 		return;
 	}
-	if (my_pos.right &&density_lookup(world[y][x+1].type) < density_lookup(world[y][x].type)) {
-		Vector2 test_vel = {2, 0};
+	if (my_pos.down_left && density_lookup(world[y + 1][x - 1].type) < density_lookup(world[y][x].type)) {
+		Vector2 test_vel = { -4, 3 };
 		move_by_velocity(world, x, y, test_vel);
 		return;
 	}
-	if (my_pos.left &&density_lookup(world[y][x-1].type) < density_lookup(world[y][x].type)) {
-		Vector2 test_vel = {-2, 0};
+	if (my_pos.right && density_lookup(world[y][x + 1].type) < density_lookup(world[y][x].type)) {
+		Vector2 test_vel = { 2, 0 };
 		move_by_velocity(world, x, y, test_vel);
 		return;
 	}
-	
-	
-	
+	if (my_pos.left && density_lookup(world[y][x - 1].type) < density_lookup(world[y][x].type)) {
+		Vector2 test_vel = { -2, 0 };
+		move_by_velocity(world, x, y, test_vel);
+		return;
+	}
+
+
+
 }
 
-void update_me(Particle **world, int x, int y, int world_size) {
+void update_me(Particle** world, int x, int y, int world_size) {
 	enum ParticleType my_type = world[y][x].type;
 	if (world[y][x].updated) {
 		world[y][x].updated = 0;
-		return ;
+		return;
 	}
 	switch (my_type) {
-		case EMPTY: break;
-		case STONE: break;
-		case AIR: break;
-		case SAND: update_powder(world, x, y, world_size); break;
-		case WATER: update_liquid(world, x, y, world_size); break;
-		case ORANGE_JUICE: update_liquid(world, x, y, world_size); break;
+	case EMPTY: break;
+	case STONE: break;
+	case AIR: break;
+	case SAND: update_powder(world, x, y, world_size); break;
+	case WATER: update_liquid(world, x, y, world_size); break;
+	case ORANGE_JUICE: update_liquid(world, x, y, world_size); break;
 	}
 }
 
-int main() {
 
+int main() {
 	InitWindow(screen_size, screen_size, "World");
 	SetTargetFPS(0);
-	
-	srand(time(NULL));
-	
-	Particle *_world = (Particle*)malloc(world_size * world_size * sizeof(Particle));
+
+	srand((int)time(NULL));
+
+	Particle* _world = (Particle*)malloc(world_size * world_size * sizeof(Particle));
 	if (!_world) return -1;
-	Particle **world = (Particle**)malloc(world_size * sizeof(Particle*));
+	Particle** world = (Particle**)malloc(world_size * sizeof(Particle*));
 	if (!world) {
 		free(_world);
 		return -1;
 	}
 	for (int i = 0; i < world_size; i++) {
-		world[i] = &(_world[i*world_size]);
+		world[i] = &(_world[i * world_size]);
 	}
 
 	generate_world(world, world_size);
-	
+
 	int mouse_x, mouse_y;
-	enum ParticleType materials[4] = {SAND, WATER, STONE, ORANGE_JUICE};
+	enum ParticleType materials[4] = { SAND, WATER, STONE, ORANGE_JUICE };
 	enum ParticleType m_choice = SAND;
-	
+
 	int l_mouse_held_last_frame = 0;
 	int r_mouse_held_last_frame = 0;
-	
+
 	Image buddyworld = LoadImage("assets/beautifu.png");
 	//ImageCrop(&buddyworld, (Rectangle){ 100, 10, 280, 380 });      // Crop an image piece
-    //ImageFlipHorizontal(&buddyworld);                              // Flip cropped image horizontally
-    ImageResize(&buddyworld, screen_size, screen_size+100);                            // Resize flipped-cropped image
+	//ImageFlipHorizontal(&buddyworld);                              // Flip cropped image horizontally
+	ImageResize(&buddyworld, screen_size, screen_size + 100);                            // Resize flipped-cropped image
 	Texture2D bg_texture = LoadTextureFromImage(buddyworld);
 	UnloadImage(buddyworld);
-	
+
 	int brush_size = 10;
 	int simulate = 1;
 
 	while (!WindowShouldClose()) {
 		mouse_x = (int)GetMouseX() / scaled_size;
 		mouse_y = (int)GetMouseY() / scaled_size;
-		
+
 		if (IsKeyPressed(KEY_ENTER)) simulate = (1 - simulate);
 		if (IsKeyPressed(KEY_ONE)) m_choice = materials[0];
-		else if (IsKeyPressed(KEY_TWO)) {m_choice = materials[1];}
+		else if (IsKeyPressed(KEY_TWO)) { m_choice = materials[1]; }
 		else if (IsKeyPressed(KEY_THREE)) m_choice = materials[2];
 		else if (IsKeyPressed(KEY_FOUR)) m_choice = materials[3];
 		else if (IsKeyPressed(KEY_J)) generate_world(world, world_size);
@@ -414,34 +410,34 @@ int main() {
 		else {
 			r_mouse_held_last_frame = 0;
 		}
-		
+
 		//TODO: change the collision to work based on speed
-		
+
 
 		BeginDrawing();
 		ClearBackground(DARKGRAY);
-		
-		DrawTexture(bg_texture, screen_size/2 - bg_texture.width/2, screen_size/2 - bg_texture.height/2 - 40, WHITE);
-		
+
+		DrawTexture(bg_texture, screen_size / 2 - bg_texture.width / 2, screen_size / 2 - bg_texture.height / 2 - 40, WHITE);
+
 		if (simulate)
 		{
-			for (int i = world_size-1; i > 0 ; --i) {
+			for (int i = world_size - 1; i > 0; --i) {
 				if (i % 2 == 0) {
 					for (int j = 0; j < world_size; j++) {
 						update_me(world, j, i, world_size);
 					}
 				}
 				else {
-					for (int j = world_size-1; j > 0; j--) {
+					for (int j = world_size - 1; j > 0; j--) {
 						update_me(world, j, i, world_size);
 					}
 				}
-				
+
 			}
 		}
 		for (int i = 0; i < world_size; i++) {
 			for (int j = 0; j < world_size; j++) {
-				DrawRectangle(j*scaled_size, i*scaled_size, scaled_size, scaled_size, world[i][j].color);
+				DrawRectangle(j * scaled_size, i * scaled_size, scaled_size, scaled_size, world[i][j].color);
 			}
 		}
 
