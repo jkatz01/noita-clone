10560e5629ac33499ad9b9870ba9dd2ce5e68f79
Rodentsnipe <rodentsnipe@gmail.com>
2026-06-07 17:43:21 +0100

    removed glad since not using, fixes it

--- src/SandWorldRenderer.hpp
+++ src/SandWorldRenderer.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <raylib.h>
 #include <rlgl.h>
-#include <glad/glad.h>
+//#include <glad/glad.h>
 #include "IntVector.hpp"
 #include "SandTile.hpp"
 #include "BufferPixels.hpp"
